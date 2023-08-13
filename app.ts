4a9cbdb88bcae42da09ed4c52f6bf802d1568568
Ussaid Imtiaz <130559788+Ussaid-Imtiaz@users.noreply.github.com>
2023-08-13 20:44:38 +0500

    Quiz App

---
+++ app.ts
@@ -0,0 +1,53 @@
+//please run the compiler from ./calcScore file as node calcScore.js to run the quiz app...
+
+
+import inquirer from "inquirer";
+
+
+export const userMCQs = await inquirer.prompt([
+  {
+    name: "one",
+    type: "checkbox",
+    message: "1).  In JavaScript, which keyword is used to declare a variable?",
+    choices: ["define", "let", "var", "set"],
+  },
+  {
+    name: "two",
+    type: "checkbox",
+    message:
+      "2).  Which of the following CSS selectors targets elements with the class name 'example'?",
+    choices: ["#example", ".example", "example", "*example"],
+  },
+  {
+    name: "three",
+    type: "checkbox",
+    message: "3).  What does the acronym 'HTML' stand for?",
+    choices: [
+      "Hyper Transfer Markup Language",
+      "Hyper Text Markup Language",
+      "High Text Markup Language",
+      "Hyperlink and Text Markup Language",
+    ],
+  },
+  {
+    name: "four",
+    type: "checkbox",
+    message: "4).  What is the main purpose of using TypeScript?",
+    choices: [
+      "To write server-side code",
+      "To add static typing to JavaScript",
+      "To create 3D graphics",
+      "To design user interfaces",
+    ],
+  },
+  {
+    name: "five",
+    type: "checkbox",
+    message: "5).  Which of the following is a valid data type in JavaScript?",
+    choices: ["Complex", "Vector", "Float", "Object"],
+  },
+]);
+
+
+
+
---
+++ calcScore.ts
@@ -0,0 +1,87 @@
+
+import chalk from "chalk";
+import { userMCQs } from "./app.js";
+
+
+
+
+function calcScore () {}
+    let userCounter = 0;
+
+if (userMCQs.one == "let") {
+  console.log(
+    chalk.bgYellowBright.greenBright.bold (
+      `Well Done! "${userMCQs.one}" is the Correct Answer`
+    )
+  );
+  userCounter++;
+} else {
+  console.log(
+    chalk.bgWhiteBright.redBright.bold (
+      `"Ooopss! ${userMCQs.one} is the Wrong Answer`
+    )
+  );
+}
+
+if (userMCQs.two == ".example") {
+  console.log(
+    chalk.bgYellowBright.greenBright.bold (
+      `Well Done! "${userMCQs.two}" is the Correct Answer`
+    )
+  );
+  userCounter++;
+} else {
+    console.log(
+      chalk.bgWhiteBright.redBright.bold(
+        `"Ooopss! ${userMCQs.two} is the Wrong Answer`
+      )
+    );
+}
+
+if (userMCQs.three == "Hyper Text Markup Language") {
+  console.log(
+    chalk.bgYellowBright.greenBright.bold (
+      `Well Done! "${userMCQs.three}" is the Correct Answer`
+    )
+  );
+  userCounter++;
+} else {
+    console.log(
+      chalk.bgWhiteBright.redBright.bold(
+        `"Ooopss! ${userMCQs.three} is the Wrong Answer`
+      )
+    );
+}
+
+if (userMCQs.four == "To add static typing to JavaScript") {
+  console.log(
+    chalk.bgYellowBright.greenBright.bold (
+      `Well Done! "${userMCQs.four}" is the Correct Answer`
+    )
+  );
+  userCounter++;
+} else {
+    console.log(
+      chalk.bgWhiteBright.redBright.bold(
+        `"Ooopss! ${userMCQs.four} is the Wrong Answer`
+      )
+    );
+}
+
+if (userMCQs.five == "Object") {
+  console.log(
+    chalk.bgYellowBright.greenBright.bold (
+      `Well Done! "${userMCQs.five}" is the Correct Answer`
+    )
+  );
+  userCounter++;
+} else {
+    console.log(
+      chalk.bgWhiteBright.redBright.bold(
+        `"Ooopss! ${userMCQs.five} is the Wrong Answer`
+      )
+    );
+}
+
+console.log(`Your Total Score is ${userCounter} out of 5`);
+
